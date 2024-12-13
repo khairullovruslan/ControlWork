7450d542a05cc443e1e72cd6381e13dda22a999d
khairullovruslan <khairullov.ruslan@gmail.com>
2024-12-13 17:25:28 +0300

    feat: added properties

--- src/main/java/ru/kpfu/itis/khairullovruslan/util/DatabaseConnectionUtil.java
+++ src/main/java/ru/kpfu/itis/khairullovruslan/util/DatabaseConnectionUtil.java
@@ -1,10 +1,15 @@
 package ru.kpfu.itis.khairullovruslan.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class DatabaseConnectionUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(DatabaseConnectionUtil.class);
 
     private DatabaseConnectionUtil() {
     }
@@ -13,18 +18,32 @@ public final class DatabaseConnectionUtil {
 
 
     public static Connection getConnection() {
+        String PROD_DB_HOST = System.getenv("PROD_DB_HOST");
+        String PROD_DB_PORT = System.getenv("PROD_DB_PORT");
+        String PROD_DB_PASSWORD = System.getenv("PROD_DB_PASSWORD");
+        String PROD_DB_NAME = System.getenv("PROD_DB_NAME");
+        String PROD_DB_USERNAME = System.getenv("PROD_DB_USERNAME");
         try {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/oris-cwork",
-                    "postgres",
-                    "postgres"
+                    "jdbc:postgresql://%s:%s/%s"
+                            .formatted(PROD_DB_HOST, PROD_DB_PORT, PROD_DB_NAME),
+                    PROD_DB_USERNAME,
+                    PROD_DB_PASSWORD
             );
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+
+
+        } catch(SQLException | ClassNotFoundException e){
+                log.error(System.getenv("PROD_DB_HOST"));
+                log.error(System.getenv("PROD_DB_PORT"));
+                log.error(System.getenv("PROD_DB_PASSWORD"));
+                log.error(System.getenv("PROD_DB_NAME"));
+                log.error(System.getenv("PROD_DB_USERNAME"));
+                throw new RuntimeException(e);
+            }
+
+            return connection;
         }
 
-        return connection;
-    }
 
-}
+    }
